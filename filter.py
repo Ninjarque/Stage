195b1cbd6fcf588077d0826e8f428737217dcfdf
Ninjarque <ninjarque@gmail.com>
2024-05-21 18:44:30 +0200

    Simple implementations of select and format for now, we'll see about them being more fledged out later on

--- filter.py
+++ filter.py
@@ -1,4 +1,5 @@
 import math
+import dichotomy
 
 FILTER_VALIDATION_THRESHOLD = 0.5
 
@@ -10,10 +11,19 @@ class Filter:
         self.validation_threshold = validation_threshold
 
     def select(self, curve, x_start, x_end):
-        pass
+        c_start = dichotomy.nearest_index(x_start, curve)
+        c_end = dichotomy.nearest_index(x_end, curve)
+        if c_start < 0 or c_end >= len(curve):
+            return []
+        return curve[c_start:c_end]
 
-    def normalize(self, curve):
-        pass
+    # actually, we'll see about centering depending on the format of the curve -> no format, no centering
+    # but we need to resize it to be "normalized" in terms of width, we'll see about that though
+    def format(self, curve):
+        m = max(curve)
+        normalized = [x / m for x in curve]
+
+        return normalized
 
 
     def apply(self, curve):
@@ -23,7 +33,7 @@ class Filter:
         curve = self.select(curve, self.x_start, self.x_end)
         if not curve:
             return False
-        curve = self.normalize(curve)
+        curve = self.format(curve)
         flt = self.function(curve)
 
         return flt > self.validation_threshold
