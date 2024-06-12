bbe929722e88dd305681673775601f1863b9de86
Ninjarque <ninjarque@gmail.com>
2024-06-13 01:35:47 +0200

    Uodate the theme to be serializable for project saving and loading

--- theme.py
+++ theme.py
@@ -14,6 +14,18 @@ class Theme:
             return self.palettes[object]
         print("Theme error, no palette found for '", object, "'!")
         return self.error_palette
+    
+    def to_dict(self):
+        return {
+            "palettes": {k: v.to_dict() for k, v in self.palettes.items()},
+            "error_palette": self.error_palette.to_dict()
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        palettes = {k: ColorPalette.from_dict(v) for k, v in data.get("palettes", {}).items()}
+        error_palette = ColorPalette.from_dict(data.get("error_palette", {}))
+        return cls(palettes=palettes, error_palette=error_palette)
 
 class ColorPalette:
     def __init__(self, colors, properties_default_colors = {}, error_color = "darkviolet"):
@@ -32,3 +44,18 @@ class ColorPalette:
                 return self.properties_default_colors[property]
         print("Unknown color arrangement ", property, " for state {", state, "}")
         return self.error_color
+    
+    def to_dict(self):
+        return {
+            "colors": self.colors,
+            "properties_default_colors": self.properties_default_colors,
+            "error_color": self.error_color
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            colors=data.get("colors", {}),
+            properties_default_colors=data.get("properties_default_colors", {}),
+            error_color=data.get("error_color", "darkviolet")
+        )
