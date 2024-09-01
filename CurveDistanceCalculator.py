5b456078391fb4b36b4e354458ca08caae23bc37
Ninjarque <ninjarque@gmail.com>
2024-09-01 14:42:37 +0200

    Started making the stats dialog work as it now tries to compute the distance, not quiet yet as it outputs zero

--- CurveDistanceCalculator.py
+++ CurveDistanceCalculator.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.interpolate import interp1d
 
 class CurveDistanceCalculator:
     @staticmethod
@@ -16,14 +15,18 @@ class CurveDistanceCalculator:
         - y1_interp: The y-values of the first curve after interpolation (if needed).
         - y2_interp: The y-values of the second curve after interpolation (if needed).
         """
+        # Determine the number of points to interpolate
         if len(x1) > len(x2):
-            x_common = x1
-            y2_interp = interp1d(x2, y2, kind='linear', fill_value='extrapolate')(x_common)
-            y1_interp = y1
+            indexes_count = len(x1)
         else:
-            x_common = x2
-            y1_interp = interp1d(x1, y1, kind='linear', fill_value='extrapolate')(x_common)
-            y2_interp = y2
+            indexes_count = len(x2)
+        
+        # Generate common x-values using linspace
+        x_common = np.linspace(min(x1[0], x2[0]), max(x1[-1], x2[-1]), indexes_count)
+        
+        # Interpolate y-values for both curves
+        y1_interp = np.interp(x_common, x1, y1)
+        y2_interp = np.interp(x_common, x2, y2)
         
         return x_common, y1_interp, y2_interp
     
@@ -40,6 +43,7 @@ class CurveDistanceCalculator:
         - The RMSE value.
         """
         x_common, y1_interp, y2_interp = CurveDistanceCalculator.interpolate_curve(x1, y1, x2, y2)
+        print(y1_interp, y2_interp)
         
         # Calculate RMSE
         mse = np.mean((y1_interp - y2_interp) ** 2)
--- StatsDialog.py
+++ StatsDialog.py
@@ -7,6 +7,8 @@ import dichotomy
 
 from FeatureExtractor import *
 
+from CurveDistanceCalculator import *
+
 class StatsDialog:
     def __init__(self, project):
         self.project = project
@@ -17,6 +19,38 @@ class StatsDialog:
 
         self.window = tk.Toplevel()
         self.window.title("Statistics display")
+
+        curve_names = [curve.name for curve in project.curves]
+
+        target_plot_index = 0
+        current_plot_index = 0
+
+
+        if project.target_curve != "" and project.target_curve in curve_names:
+            target_plot_index = curve_names.index(project.target_curve)
+        else:
+            print("Didn't have a proper target curve, exiting match operation...")
+            return
+        if project.current_curve != "" and project.current_curve in curve_names:
+            current_plot_index = curve_names.index(project.current_curve)
+        else:
+            print("Didn't have a proper current curve, exiting match operation...")
+            return
+        
+        target_curve = project.curves[target_plot_index]
+        current_curve = project.curves[target_plot_index]
+
+        target_curve_datax = target_curve.full_datax
+        target_curve_datay = target_curve.full_datay
+
+        current_curve_datax = current_curve.full_datax
+        current_curve_datay = current_curve.full_datay
+
+        RMSE = CurveDistanceCalculator.calculate_rmse(target_curve_datax, target_curve_datay, current_curve_datax, current_curve_datay)
+
+
+        print("RMSE:", RMSE)
+        self.RMSE_label = tk.Label(self.window, text=str(RMSE))
 
         return
 
